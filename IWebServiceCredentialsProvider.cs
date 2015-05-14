02791c76b8f2fcf929ec2cd28b77b10e7149312a
Rick Mason <richard.mason@eastsussex.gov.uk>
2015-05-14 12:44:56 +0100

    Add common code for setting web service credentials

---
+++ IWebServiceCredentialsProvider.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net;
+using System.Text;
+
+namespace EsccWebTeam.Data.Xml
+{
+    /// <summary>
+    /// Provides credentials for accessing web services
+    /// </summary>
+    public interface IWebServiceCredentialsProvider
+    {
+        /// <summary>
+        /// Creates the credentials.
+        /// </summary>
+        /// <returns></returns>
+        ICredentials CreateCredentials();
+    }
+}
